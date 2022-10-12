8764a338c8e86c517a5d201538cf4a1242d55166
Thyago Ribeiro <thyagoribeiro608@gmail.com>
2022-10-12 15:21:38 -0400

    Added Component Heading and Stories

--- src/components/Heading.stories.tsx
+++ src/components/Heading.stories.tsx
@@ -1,11 +1,11 @@
 import { Meta, StoryObj } from "@storybook/react";
-import { Text, TextProps } from "./Text";
+import { Heading, HeadingProps } from "./Heading";
 
 export default {
-  title: "Components/Text",
-  component: Text,
+  title: "Components/Heading",
+  component: Heading,
   args: {
-    children: "Text Component",
+    children: "Heading Component",
     size: "md",
   },
   argTypes: {
@@ -16,26 +16,26 @@ export default {
       },
     },
   },
-} as Meta<TextProps>;
+} as Meta<HeadingProps>;
 
-export const Default: StoryObj<TextProps> = {};
+export const Default: StoryObj<HeadingProps> = {};
 
-export const Small: StoryObj<TextProps> = {
+export const Small: StoryObj = {
   args: {
     size: "sm",
   },
 };
 
-export const Large: StoryObj<TextProps> = {
+export const Large: StoryObj<HeadingProps> = {
   args: {
     size: "lg",
   },
 };
 
-export const CustomComponent: StoryObj<TextProps> = {
+export const CustomComponent: StoryObj<HeadingProps> = {
   args: {
     asChild: true,
-    children: <p>Componente Customizado</p>,
+    children: <h1>Heading Component with h1</h1>,
   },
   argTypes: {
     children: {
